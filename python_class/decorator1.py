2bc5822a7c91c44059f4cfad4d04f5e62f50fd75
ramanbodh <crazybodh14@gmail.com>
2026-08-11 14:09:44 +0530

    passing function to another function

--- python_class/decorator1.py
+++ python_class/decorator1.py
@@ -7,3 +7,11 @@ print(type(greeting))#type of the object greeting will be funct
 
 hello=greeting#function object reference is passed to another variable"
 hello()#fuction execution
+
+print("passing function to another function")
+def execute(func):
+    print("startion...")
+    func()
+    print("ending....")
+
+execute(greeting)
