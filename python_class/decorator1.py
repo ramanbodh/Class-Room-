7cc32bc683fdc983a2cc19bf63d241be0e43d1f4
ramanbodh <crazybodh14@gmail.com>
2026-08-11 14:03:35 +0530

    function object reference is passed to another variable

--- python_class/decorator1.py
+++ python_class/decorator1.py
@@ -1,6 +1,9 @@
 def greeting():
     print("hello")
 
-print(greeting)
-greeting()
-print(type(greeting))
+print(greeting)#identity not exactly address
+greeting()#execution of the function code
+print(type(greeting))#type of the object greeting will be funct
+
+hello=greeting#function object reference is passed to another variable"
+hello()#fuction execution
