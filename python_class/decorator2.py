14add3a09f5b8b47ce00a4f123c2707794260e8e
ramanbodh <crazybodh14@gmail.com>
2026-08-14 20:06:25 +0530

    program of validation minimum value using factory decorator without @ symbol

--- python_class/decorator2.py
+++ python_class/decorator2.py
@@ -22,3 +22,22 @@ def greeting1(name):
 
 greeting1=repeat(4)(greeting1)
 greeting1("RAM")
+
+print("minimum value validator program\n\n")
+def minimum(number):
+    def decorator(funct):
+        def wrapper(value):#here value is passed directly without storing it in tuple and dictionary like *args,**kwargs
+            if value < number:
+                print("to small value")
+                return
+            return funct(value)
+        return wrapper
+    return decorator
+
+print(" lets try without using @ symbol first")
+
+def num1(value):
+    return f"value is {value}"
+num1=minimum(5)(num1)
+result=num1(10)
+print(result)
